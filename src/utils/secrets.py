269a348971a94c6be46de6d66f7271cd2efaa2b4
Alexander Medina <alexjmedina@users.noreply.github.com>
2025-10-30 18:14:46 -0400

    Update secrets.py

--- src/utils/secrets.py
+++ src/utils/secrets.py
@@ -1,10 +1,15 @@
-import boto3, json, os
-from botocore.config import Config
+import os, json, boto3
 
-_region = os.environ.get("REGION", "us-east-1")
-_arn    = os.environ["SECRETS_ARN"]
-_sm     = boto3.client("secretsmanager", config=Config(region_name=_region))
-
-def get_secrets():
-    val = _sm.get_secret_value(SecretId=_arn)
-    return json.loads(val["SecretString"])
+def get_twilio_secrets(secret_name=None, region=None):
+    name   = secret_name or os.getenv("TWILIO_SECRET_NAME", "payslice/twilio/txn")
+    region = region or os.getenv("AWS_REGION", "us-east-1")
+    sm = boto3.client("secretsmanager", region_name=region)
+    s = sm.get_secret_value(SecretId=name)
+    raw = s.get("SecretString") or s["SecretBinary"].decode()
+    data = json.loads(raw)
+    return {
+        "account_sid": data["TWILIO_ACCOUNT_SID"],
+        "auth_token":  data["TWILIO_AUTH_TOKEN"],
+        "msid":        data.get("TWILIO_MESSAGING_SERVICE_SID") or data["TWILIO_MSID"],
+        "bearer":      data.get("INTERNAL_BEARER") or data["INTERNAL_BEARER_TOKEN"],
+    }
